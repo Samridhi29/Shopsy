13b3ff4077b828590786505d6d0bcd579be9ed66
samridhi29 <jhasamridhi29@gmail.com>
2025-06-25 22:20:19 +0530

    added payment and analytics route

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express";  //in package.json, in type we wrote module, so we are importing using this syntax and not const..
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import path from "path";
+//import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
-// import paymentRoutes from "./routes/payment.route.js";
-// import analyticsRoutes from "./routes/analytics.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -26,8 +26,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
-// app.use("/api/payments", paymentRoutes);
-// app.use("/api/analytics", analyticsRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 // if (process.env.NODE_ENV === "production") {
 // 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
